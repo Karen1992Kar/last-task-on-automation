7e1fd75ed60422350b3f9807266f0b2b23892d33
Karen Karagebakyan <mkaragebakyan@gmail.com>
2020-06-03 21:44:58 +0400

    Pros of inheritance: an example

--- pages/locators.py
+++ pages/locators.py
@@ -3,25 +3,29 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class BasketPageLocators():
+class BasketPageLocators:
     BASKET_CLICK = (By.CSS_SELECTOR, '#add_to_basket_form')
 
 
-class CheckNameLocators():
+class CheckNameLocators:
     PRODUCT_NAME_1 = (By.TAG_NAME, "h1")
     PRODUCT_NAME_2 = (By.CSS_SELECTOR, '#messages .alert:first-child strong')
 
 
-class ProductPageLocators():
+class ProductPageLocators:
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div.alert > div > strong")
 
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 import pytest
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -24,23 +24,34 @@ link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 #     # page1.should_not_be_success_message()
 #     page1.should_be_disappeared()
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_basket_page()
-    page.should_not_be_success_message()
+    page.should_be_login_link()
 
-
-def test_guest_cant_see_success_message(browser):
+def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
-    page.should_not_be_success_message()
+    page.go_to_login_page()
 
 
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    page.should_be_disappeared()
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_basket_page()
+#     page.should_not_be_success_message()
+#
+#
+# def test_guest_cant_see_success_message(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_not_be_success_message()
+#
+#
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_basket_page()
+#     page.should_be_disappeared()
 
 
